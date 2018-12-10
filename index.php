52caa0d3452939e8a00d9bb837cf6ead232506b1
bop001 <sashaburlachenko@gmail.com>
2018-12-10 17:08:58 +0200

    change button name

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		Красный цвет:<input type="text" name="red"><br><br>
 		Зеленый цвет:<input type="text" name="green"><br><br>
 		Синий цвет:<input type="text" name="blue"><br><br>
-		<input type="submit" value="Просмотреть"><br><br>
+		<input type="submit" value="Accept"><br><br>
 		<span>Тестовый текст...</span>
 	</form>
 

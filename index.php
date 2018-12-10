398441de76a75bf36c79137d8bbcb3c846575bb0
bop001 <sashaburlachenko@gmail.com>
2018-12-10 16:55:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>HW_PHP1</title>
+</head>
+<body>
+	
+
+	<form action="">
+		Красный цвет:<input type="text" name="red"><br><br>
+		Зеленый цвет:<input type="text" name="green"><br><br>
+		Синий цвет:<input type="text" name="blue"><br><br>
+		<input type="submit" value="Просмотреть"><br><br>
+		<span>Тестовый текст...</span>
+	</form>
+
+</body>
+</html>

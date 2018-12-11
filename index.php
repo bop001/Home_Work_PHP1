54fc21eaaea759ab060757e46bcd92bb0e2b1175
bop001 <sashaburlachenko@gmail.com>
2018-12-11 11:42:14 +0200

    working background

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+	/*$red = function (){
+		if('red'>= 0 || 'red'<=255){
+			$_POST['red'];
+		}else {
+			echo "Введите правильное число";
+		}		
+	};
+*/	
+	$red = $_POST['red'];
+	$green = $_POST['green'];
+
+	$blue = $_POST['blue'];
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -5,14 +20,12 @@
 	<title>HW_PHP1</title>
 </head>
 <body>
-	
-
-	<form action="">
-		Красный цвет:<input type="text" name="red"><br><br>
-		Зеленый цвет:<input type="text" name="green"><br><br>
-		Синий цвет:<input type="text" name="blue"><br><br>
+	<form action="/" method="post">
+		Красный цвет: <input type="text" name="red"  method="post"><br><br>
+		Зеленый цвет: <input type="text" name="green"  method="post"><br><br>
+		Синий цвет: <input type="text" name="blue"  method="post"><br><br>
 		<input type="submit" value="Accept"><br><br>
-		<span>Тестовый текст...</span>
+		<span style="background-color: rgb(<?php echo $red; ?>, <?php echo $green; ?>, <?php echo $blue; ?>)">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae labore hic rem cumque. Illum dolorum soluta doloremque, deleniti excepturi fugiat, ut ex provident sunt libero voluptatem quo tempora, inventore, molestiae.</span>
 	</form>
 
 </body>

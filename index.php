93c35f6031a441c7bfb8ee773cb91c220fe660f0
bop001 <sashaburlachenko@gmail.com>
2018-12-15 10:34:46 +0200

    working bad

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	$blue = $_POST['blue'];
 	$colorText = "#000";
 	if (!empty($_POST['red']) and !empty($_POST['green']) and !empty($_POST['blue']) and $colorText == "#000") {
-		$colorText = "#fff"; //не понял как чтобы не окрашевалась, когда не пустой запрос, чтобы в одну переменную, с запятыми...
+		$colorText = "#fff"; // чтобы в одну переменную, с запятыми...
 	}
 ?>
 
@@ -56,11 +56,70 @@
 		</form>
 	</div>
 	<?php
-		$month_num = $_POST['month'];
-		if (!empty($month_num)) {
-			echo $month_num;
+		$month = $_POST['month'];
+		if (!empty($month)) {
+			calendar($month);
 		}
-		
+			/*$month['1'] = 'Январь';
+			$month['2'] = 'Февраль';
+			$month['3'] = 'Март';
+			$month['4'] = 'Апрель';
+			$month['5'] = 'Май';
+			$month['6'] = 'Июнь';
+			$month['7'] = 'Июль';
+			$month['8'] = 'Август';
+			$month['9'] = 'Сентябрь';
+			$month['10'] = 'Октябрь';
+			$month['11'] = 'Ноябрь';
+			$month['12'] = 'Декабрь';*/
+			$year = date('Y');
+
+		function calendar($month){
+			global $month, $year;
+
+			$dayofmonth = date('t');
+			$day_count = 1;
+			$num = 0;
+
+		  	for($i = 0; $i < 7; $i++){
+		  		$dayofweek = date('w', mktime(0, 0, 0, $month, $day_count, $year));		  	
+    			$dayofweek = $dayofweek - 1;
+
+	    		if($dayofweek == -1) $dayofweek = 6;
+			    if($dayofweek == $i){
+			   		$week[$num][$i] = $day_count;
+			   		$day_count++;
+			    }
+			    else{
+			      $week[$num][$i] = "";
+				}
+			}
+			while(true){
+				$num++;
+			for($i = 0; $i < 7; $i++){
+				$week[$num][$i] = $day_count;
+				$day_count++;
+      		if($day_count > $dayofmonth) break;
+			}
+		    if($day_count > $dayofmonth) break;
+		  	}
+
+	echo "<br><br><table border=1>";
+  		for($i = 0; $i < count($week); $i++){
+    echo "<tr>";
+    	for($j = 0; $j < 7; $j++){
+	    	if(!empty($week[$i][$j])){
+        	if($j == 5 || $j == 6) 
+            	echo "<td><font color=red>".$week[$i][$j]."</font></td>";
+       		else echo "<td>".$week[$i][$j]."</td>";
+       		}else echo "<td>&nbsp;</td>";
+	   	}
+   	echo "</tr>";
+  		} 
+ 	echo "</table>";
+ 	}	
+
+
 	?>
 </body>
 </html>
